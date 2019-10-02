c13b148d9c9603ae702029c17b14124058c9c149
riverswon <snowsnow12199112@gmail.com>
2019-10-02 12:23:36 +0900

    fix

--- normalization.py
+++ normalization.py
@@ -1,28 +1,18 @@
-#normalization of 1,...,5.wav by [-1:1]
-#in  1,...,5.wav
-#out norm1,...,5.wav
+# normalization [-1, 1]
 
 import librosa
 import numpy as np
 
-#normalization of 1,...5.wav by [-1:1]
-def norm1(in_path, out_path, fs):
-    x, f = librosa.load(in_path, sr=fs)
-    x_max = max(x)
-    x_min = min(x)
-    for i in range(len(x)):
-        x[i] = (x[i] - x_min) / (x_max - x_min) * 2.0 - 1.0
-    librosa.output.write_wav(out_path, x, fs)
+def norm(x):
+	x_max = max(x)
+	x_min = min(x)
+	x[i] = (x[i] - x_min) / (x_max - x_min) * 2.0 - 1.0
+	return x
 
-def frog_normalization():
-    fs = 44100
-    frog_sample = 5
-    for i in range(frog_sample):
-        norm1('original_data/frog'+str(i+1)+'.wav', 'test_data/frog'+str(i+1)+'.wav', fs)
-
-def bat_normalization():
-    fs = 44100
-    norm1('original_data/bat.wav', 'test_data/bat.wav', fs)
-
-#frog_normalization()
-#bat_normalization()
+if __name__ == "__main__":
+	inpath = 'data/...'
+	outpath = 'data/...'
+	fs = 44100
+	x, f = librosa.load(in_path, sr=fs)
+	x = norm(x)
+	librosa.output.write_wav(out_path, x, fs)
